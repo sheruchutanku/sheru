fadf8966ea6b898f668b2146e2feefd969bf5a61
sheruchutanku <harish.narang2020@gmail.com>
2025-03-30 16:46:47 +0530

    Update provider.tf

--- AKS/provider.tf
+++ AKS/provider.tf
@@ -9,18 +9,20 @@ terraform {
       version = ">= 3.40.0"
     }
     random = {
-      version = ">=3.0"
+      version = ">= 3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 2.34.0"
     }
   }
-backend "azurerm" {
+
+  backend "azurerm" {
     resource_group_name   = "my-backend-rg"
     storage_account_name  = "myterraformstorage"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
 }
+
 

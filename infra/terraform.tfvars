9a0342267234a4525bd1aab97feaf983f5e74df3
root <root@test.1cooywu2lt2ezlcdmln5sbwamg.cx.internal.cloudapp.net>
2025-04-18 07:22:50 +0000

    t10

---
+++ infra/terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "__AKS_RESOURCE_GROUP__"
+cluster_name        = "__AKS_NAME__"
+acr_name            = "__ACR_NAME__"
---
+++ infra/var.tf
@@ -0,0 +1,28 @@
+variable "node_count" {
+  default = 1
+}
+
+variable "dns_prefix" {
+  default = "aks-k8s-2022"
+}
+
+variable "cluster_name" {
+  default = "aks-k8s-2022"
+}
+
+variable "kubernetes_version" {
+  default = "1.21.2"
+}
+
+variable "acr_name" {
+  default = "acrforaks2022"
+}
+
+
+variable "resource_group_name" {
+  default = "aks-k8s-2020"
+}
+
+variable "location" {
+  default = "eastus2
+}

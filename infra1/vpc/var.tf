5ac143bf67e8271825aeda95963279a8f862fdae
root <root@test.1cooywu2lt2ezlcdmln5sbwamg.cx.internal.cloudapp.net>
2025-04-23 14:13:28 +0000

    t1

---
+++ infra1/vpc/var.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "name_prefix" {
+  default = "demo"
+}
+

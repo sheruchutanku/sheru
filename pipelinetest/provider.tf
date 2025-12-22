17dc07a15d3f5513ca93937b1f8df1a888100529
Your Name <you@example.com>
2025-12-22 23:30:48 +0530

    t1

---
+++ pipelinetest/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"  #
+      version = ">= 3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.34.0"
+    }
+  }
+  backend "azurerm" {
+  }
+  }
+
+
+provider "azurerm" {
+  features {}
+}

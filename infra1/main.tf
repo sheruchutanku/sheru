d3712fc0b10a35f44ef753f05141f8ae9454b8ea
sheruchutanku <harish.narang2020@gmail.com>
2025-12-22 16:36:19 +0530

    Update main.tf

--- infra1/main.tf
+++ infra1/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   default_node_pool {
     name       = "systempool"
     node_count = var.node_count
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_D2_v3"
   }
 
   identity {
